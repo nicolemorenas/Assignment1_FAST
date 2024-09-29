f2cd8d86a0e3c821dc507d3587e0cdd539d88c8b
Nicole Morenas <morenas@sheridancollege.ca>
2024-09-29 14:29:25 -0400

    Project Initialized

---
+++ Assignment1_FAST/Controllers/HomeController.cs
@@ -0,0 +1,16 @@
+﻿using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using Assignment1_FAST.Models;
+
+namespace Assignment1_FAST.Controllers;
+
+public class HomeController : Controller
+{
+    public IActionResult Index()
+    {
+        return View();
+
+    }
+}
+
+
---
+++ Assignment1_FAST/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+﻿namespace Assignment1_FAST.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
+
